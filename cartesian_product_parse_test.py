e42eb0d4d4822de48d111548c86b5be0967fbae5
Jake Miles <jacob.miles@gmail.com>
2017-02-21 01:01:11 -0500

    corrected the literal curly tests - all parse tests pass

--- cartesian_product_parse_test.py
+++ cartesian_product_parse_test.py
@@ -3,9 +3,7 @@ from cartesian_product_calc import And, Or, Lit, Empty
 from cartesian_product_parse import *
 from grammar import Grammar
 
-class CartesianProductParseTest(unittest.TestCase):
-
-    Grammar.trace = True
+class ParseSaneCasesTest(unittest.TestCase):
     
     def test_create_cursor_empty_string(self):
         self.assertEqual(create_cursor(""), Cursor([]))
@@ -71,12 +69,17 @@ class CartesianProductParseTest(unittest.TestCase):
                                       Lit("d")]),
                                   Lit("y")]))
         
-    def test_nested_Or(self):
+    def test_nested_Or_on_left(self):
         expr = parse_bash_cp("{{b,c},d}ef")
         self.assertEqual(expr, And([Or([Or([Lit("b"), Lit("c")]),
                                         Lit("d")]),
                                     Lit("ef")]))
 
+    def test_nested_Or_on_right(self):
+        expr = parse_bash_cp("{b,{c,d},e}")
+        self.assertEqual(expr, Or([Lit("b"),
+                                   Or([Lit("c"), Lit("d")]),
+                                   Lit("e")]))
     def test_example_1(self):
         expr = parse_bash_cp("a{b,c}d{e,f,g}hi")
         self.assertEqual(expr, And([
@@ -101,12 +104,12 @@ class CartesianProductParseTest(unittest.TestCase):
             Or([Lit("k"), Lit("l")])
         ]))
 
-        
-class CartesianProductParseTestLiteralCurlies(unittest.TestCase):
-        
-    # when curlies don't contain a comma,
-    # they don't denote a Or but a literal that includes the curlies
 
+# when curlies don't contain a comma,
+# they don't denote a Or but a literal that includes the curlies.
+# same goes for open and close curlies with no match.
+class ParseLiteralCurliesTest(unittest.TestCase):
+        
     def test_literal_open_curly(self):
         expr = parse_bash_cp("{")
         self.assertEqual(expr, Lit("{"))
@@ -117,39 +120,40 @@ class CartesianProductParseTestLiteralCurlies(unittest.TestCase):
 
     def test_literal_open_close_curly(self):
         expr = parse_bash_cp("{}")
-        self.assertEqual(expr, Lit("{}"))
+        self.assertEqual(expr, And([Lit("{"), Lit("}")]))
 
     def test_empty_curlies_after_literal(self):
         expr = parse_bash_cp("a{}b")
-        self.assertEqual(expr, Lit("a{}b"))
+        self.assertEqual(expr, And([Lit("a"), Lit("{"), Lit("}"), Lit("b")]))
 
     def test_literal_curlies_containing_subexpr(self):
         expr = parse_bash_cp("a{b{c,d}}")
-        self.assertEqual(expr, And([Lit("a{b"), Or([Lit("c"), Lit("d")]), Lit("}")]))
+        self.assertEqual(expr, And([Lit("a"), Lit("{"), Lit("b"), Or([Lit("c"), Lit("d")]), Lit("}")]))
 
     def test_Or_containing_literal_curly_expression(self):
+        # this one's wacky - the first close curly closes the Or.  the second is Lit.
         expr = parse_bash_cp("{b,{cd}}")
-        self.assertEqual(expr, Or([Lit("b"), Lit("{cd}")]))
+        self.assertEqual(expr, And([Or([Lit("b"), And([Lit("{"), Lit("cd")])]), Lit("}")]))
 
     def test_literal_open_curly_then_Or(self):
         expr = parse_bash_cp("a{{b,c}")
-        self.assertEqual(expr, And([Lit("a{"), Or([Lit("b"), Lit("c")])]))
+        self.assertEqual(expr, And([Lit("a"), Lit("{"), Or([Lit("b"), Lit("c")])]))
 
-    def test_literal_open_curly_and_faux_empty_branch(self):
+    def test_literal_open_curly_faux_empty_branch(self):
         expr = parse_bash_cp("abc{,de")
-        self.assertEqual(expr, Lit("abc{,de"))
+        self.assertEqual(expr, And([Lit("abc"), Lit("{"), Lit(","), Lit("de")]))
 
     def test_literal_open_curly_and_faux_branch(self):
         expr = parse_bash_cp("ab{c,de")
-        self.assertEqual(expr, Lit("abc{,de"))
+        self.assertEqual(expr, And([Lit("ab"), Lit("{"), Lit("c"), Lit(","), Lit("de")]))
                          
     def test_second_close_curly_is_literal(self):
         expr = parse_bash_cp("ab{c,d}}")
         self.assertEqual(expr, And([Lit("ab"), Or([Lit("c"), Lit("d")]), Lit("}")]))
 
-    def test_nested_literal_close_curly_no_open(self):
+    def test_literal_close_curly_no_open(self):
         expr = parse_bash_cp("ab}{c,d}")
-        self.assertEqual(expr, And([Lit("ab}"), Or([Lit("c"), Lit("d")])]))
+        self.assertEqual(expr, And([Lit("ab"), Lit("}"), Or([Lit("c"), Lit("d")])]))
         
     def test_mix(self):
         expr = parse_bash_cp("a{b,c}{d,e}fg{h,{y,z}i,j}")
@@ -158,8 +162,11 @@ class CartesianProductParseTestLiteralCurlies(unittest.TestCase):
                                     Or([Lit("d"),Lit("e")]),
                                     Lit("fg"),
                                     Or([Lit("h"),
-                                        Or([Lit("y"), Lit("z")]), Lit("i"),Lit("j")])]))
-                    
+                                        And([Or([Lit("y"),
+                                                 Lit("z")]),
+                                             Lit("i")]),
+                                        Lit("j")])]))
+        
 if __name__ == '__main__':
     unittest.main()
     
